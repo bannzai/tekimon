8dd3ee108f2d83085c92d46740318e5da1957f48
bannzai <kingkonog999yhirose@gmail.com>
2021-03-20 17:04:35 +0900

    :memo: add endpoint

--- app/controllers/irasutoya_monsters_controller.rb
+++ app/controllers/irasutoya_monsters_controller.rb
@@ -1,2 +1,14 @@
 class IrasutoyaMonstersController < ApplicationController
+
+  def index
+    @monsters = IrasutoyaMonster.all
+    json_response(@monsters)
+  end
+
+  private
+
+  def irastuoya_monsters_params
+    params.permit(:words)
+  end
+
 end
